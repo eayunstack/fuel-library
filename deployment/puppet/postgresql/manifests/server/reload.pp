7e75423cc8044cab0b00118aa6609474f17d5914
Zhao Chao <chao.zhao@eayun.com>
2015-03-09 19:16:11 +0800

    fix postgres service puppet codes for docker instance.
    Signed-off-by: Zhao Chao <chao.zhao@eayun.com>

--- deployment/puppet/postgresql/manifests/server/reload.pp
+++ deployment/puppet/postgresql/manifests/server/reload.pp
@@ -3,11 +3,21 @@ class postgresql::server::reload {
   $service_name   = $postgresql::server::service_name
   $service_status = $postgresql::server::service_status
 
-  exec { 'postgresql_reload':
-    path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-    command     => "service ${service_name} reload",
-    onlyif      => $service_status,
-    refreshonly => true,
-    require     => Class['postgresql::server::service'],
+  if $::is_virtual == 'true' and $::virtual =~ /docker/ and $::operatingsystemmajrelease >= 7 {
+    exec { 'postgresql_reload':
+      path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+      command     => 'su - postgres -c "export PGDATA=/var/lib/pgsql/data;/usr/bin/pg_ctl reload -D \${PGDATA} -s',
+      onlyif      => $service_status,
+      refreshonly => true,
+      require     => Class['postgresql::server::service'],
+    }
+  } else {
+    exec { 'postgresql_reload':
+      path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+      command     => "service ${service_name} reload",
+      onlyif      => $service_status,
+      refreshonly => true,
+      require     => Class['postgresql::server::service'],
+    }
   }
 }
