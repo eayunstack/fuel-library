47dd3afe6465024058e3b6f0e9666eeea800ceda
Dunrong Huang <dunrong.huang@eayun.com>
2015-05-05 18:34:14 +0800

    keystone: add cron task for flush token
    Signed-off-by: Dunrong Huang <dunrong.huang@eayun.com>

--- deployment/puppet/openstack/manifests/keystone.pp
+++ deployment/puppet/openstack/manifests/keystone.pp
@@ -380,6 +380,25 @@ class openstack::keystone (
       }
       Exec <| title == 'keystone-manage db_sync' |> -> Class['ceilometer::keystone::auth']
     }
+    if $token_driver == 'keystone.token.backends.sql.Token' {
+      package { 'crontabs':
+        ensure => latest,
+      }
+      service { 'crond':
+        ensure => running,
+        enable => true,
+      }
+
+      # Flush expired tokens
+      cron { 'keystone-flush-token':
+        ensure      => present,
+        command     => 'keystone-manage token_flush',
+        environment => 'PATH=/bin:/usr/bin:/usr/sbin',
+        user        => 'root',
+        hour        => '1',
+        require     => Package['crontabs'],
+      }
+    }
   }
 
 }
