eayunstack/fuel-library

commit 5d31bf634d658f90830ef4fd62694aba9ddccee9

ceph: use ceph_cluster network for ceph cluster network if defined
Fixes: redmine #3269

Signed-off-by: huntxu <mhuntxu@gmail.com>

deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
@@ -271,11 +271,12 @@ class osnailyfacter::cluster_ha {
     $primary_mon    = $controllers[0]['name']
 
     if ($::use_neutron) {
-      $ceph_cluster_network = get_network_role_property('storage', 'cidr')
+      $tmp_ceph_cluster_network = get_network_role_property('ceph_cluster', 'cidr')
       $ceph_public_network  = get_network_role_property('storage', 'cidr')
+      $ceph_cluster_network = $tmp_ceph_cluster_network || $ceph_public_network
     } else {
-      $ceph_cluster_network = $::fuel_settings['storage_network_range']
       $ceph_public_network = $::fuel_settings['storage_network_range']
+      $ceph_cluster_network = $::fuel_settings['ceph_cluster_network_range'] || $ceph_public_network
     }
 
     class {'ceph':

deployment/puppet/osnailyfacter/manifests/cluster_simple.pp
@@ -189,11 +189,12 @@ class osnailyfacter::cluster_simple {
 
   if ($::use_ceph) {
     if ($::use_neutron) {
-      $ceph_cluster_network = get_network_role_property('storage', 'cidr')
+      $tmp_ceph_cluster_network = get_network_role_property('ceph_cluster', 'cidr')
       $ceph_public_network  = get_network_role_property('storage', 'cidr')
+      $ceph_cluster_network = $tmp_ceph_cluster_network || $ceph_public_network
     } else {
-      $ceph_cluster_network = $::fuel_settings['storage_network_range']
       $ceph_public_network = $::fuel_settings['storage_network_range']
+      $ceph_cluster_network = $::fuel_settings['ceph_cluster_network_range'] || $ceph_public_network
     }
 
     $primary_mons   = $controller

commit be863c0d6261e3f2ee083e08e3fd5d7c4601d194

ceph: fix error introduced by commit 5d31bf634
Signed-off-by: huntxu <mhuntxu@gmail.com>

deployment/puppet/osnailyfacter/manifests/cluster_ha.pp
@@ -271,12 +271,20 @@ class osnailyfacter::cluster_ha {
     $primary_mon    = $controllers[0]['name']
 
     if ($::use_neutron) {
-      $tmp_ceph_cluster_network = get_network_role_property('ceph_cluster', 'cidr')
       $ceph_public_network  = get_network_role_property('storage', 'cidr')
-      $ceph_cluster_network = $tmp_ceph_cluster_network || $ceph_public_network
+      $tmp_ceph_cluster_network = get_network_role_property('ceph_cluster', 'cidr')
+      if ($tmp_ceph_cluster_network) {
+        $ceph_cluster_network = $tmp_ceph_cluster_network
+      } else {
+        $ceph_cluster_network = $ceph_public_network
+      }
     } else {
       $ceph_public_network = $::fuel_settings['storage_network_range']
-      $ceph_cluster_network = $::fuel_settings['ceph_cluster_network_range'] || $ceph_public_network
+      if ($::fuel_settings['ceph_cluster_network_range']) {
+        $ceph_cluster_network = $::fuel_settings['ceph_cluster_network_range']
+      } else {
+        $ceph_cluster_network = $ceph_public_network
+      }
     }
 
     class {'ceph':

deployment/puppet/osnailyfacter/manifests/cluster_simple.pp
@@ -189,12 +189,20 @@ class osnailyfacter::cluster_simple {
 
   if ($::use_ceph) {
     if ($::use_neutron) {
-      $tmp_ceph_cluster_network = get_network_role_property('ceph_cluster', 'cidr')
       $ceph_public_network  = get_network_role_property('storage', 'cidr')
-      $ceph_cluster_network = $tmp_ceph_cluster_network || $ceph_public_network
+      $tmp_ceph_cluster_network = get_network_role_property('ceph_cluster', 'cidr')
+      if ($tmp_ceph_cluster_network) {
+        $ceph_cluster_network = $tmp_ceph_cluster_network
+      } else {
+        $ceph_cluster_network = $ceph_public_network
+      }
     } else {
       $ceph_public_network = $::fuel_settings['storage_network_range']
-      $ceph_cluster_network = $::fuel_settings['ceph_cluster_network_range'] || $ceph_public_network
+      if ($::fuel_settings['ceph_cluster_network_range']) {
+        $ceph_cluster_network = $::fuel_settings['ceph_cluster_network_range']
+      } else {
+        $ceph_cluster_network = $ceph_public_network
+      }
     }
 
     $primary_mons   = $controller